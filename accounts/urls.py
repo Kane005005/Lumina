23c2c99489e91835581379ac9f8e336d9dbe092b
Kane005005 <traorekanz@gmail.com>
2026-04-26 17:17:58 +0000

    acounts

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,11 +2,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import UserProfileViewSet, TransactionViewSet
+from rest_framework.urlpatterns import format_suffix_patterns
 
 router = DefaultRouter()
 router.register(r'profile', UserProfileViewSet, basename='profile')
 router.register(r'transactions', TransactionViewSet, basename='transaction')
 
 urlpatterns = [
+    # Routes standards
     path('', include(router.urls)),
 ]
